fa678b85d0e92b2eb60afffdf2bdea5c21f2da4d
Kavita Mahato <kavitamahato0835@gmail.com>
2026-05-27 19:30:37 +0530

    Login API created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,38 +1,32 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
-// User register controller
-// POST: /api/auth/register
-
+// User Register Controller - POST: /api/auth/register
 async function userRegisterController(req, res) {
     try {
-        const { name, password, email } = req.body;
+        const { email, password, name } = req.body;
 
-        if (!name || !password || !email) {
+        if (!email || !password || !name) {
             return res.status(400).json({
                 message: 'Name, email, and password are required.',
                 status: 'Failed',
             });
         }
 
-        const isExists = await userModel.findOne({
-            email: email,
-        });
+        const isExists = await userModel.findOne({email: email,});
+
         if (isExists) {
             return res.status(422).json({
                 message: 'User already exists.',
                 status: 'Failed',
             });
         }
-
         const user = await userModel.create({
-            name,
-            password,
             email,
+            password,
+            name,
         });
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '3d',
-        });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: '3d',});
 
         res.cookie('token', token);
 
@@ -45,7 +39,6 @@ async function userRegisterController(req, res) {
             token,
         });
     } catch (error) {
-        console.error(error);
         res.status(500).json({
             message: 'Server error during registration.',
             error: error.message,
@@ -53,6 +46,46 @@ async function userRegisterController(req, res) {
     }
 }
 
+// User login Controller - POST: /api/auth/login
+async function userLoginController(req, res) {
+    try{
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email}).select("+password");
+        if (!user) {
+            return res.status(401).json({
+                message: "Email or Password is INVALID"
+            })
+        }
+        const isValidPassword = await user.comparePassword(password);
+
+        if (!isValidPassword) {
+            return res.status(401).json({
+                message: "Email or Password is INVALID"
+            })
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: '3d',});
+
+        res.cookie('token', token);
+
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Unable to Login',
+            error: error.message,
+        });
+    }
+}
+
 module.exports = {
     userRegisterController,
+    userLoginController
 };
